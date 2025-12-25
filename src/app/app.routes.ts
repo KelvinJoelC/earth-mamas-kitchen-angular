2e790ada4a96ceb181fe72d3cf3e941b8d1513b2
Kelvin Carreno <kelvinjcarreno@gmail.com>
2025-12-25 13:37:59 +1000

    add main layout with header component and routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,15 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/home-page/home-page').then(m => m.HomePage),
+    loadComponent: () => import('./layouts/main-layout/main-layout').then(m => m.MainLayout),
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/home-page/home-page')
+            .then(m => m.HomePage)
+      }
+    ]
   },
   {
     path: '**',
