533a07e92abc3ef8b39ff474ac16d503040de422
Kelvin Carreno <kelvinjcarreno@gmail.com>
2025-12-28 12:36:23 +1000

    Refactor SUBSCRIBE to ASYNCPIPE !! Moder Form

--- src/app/pages/catalog-item-page/catalog-item-page.ts
+++ src/app/pages/catalog-item-page/catalog-item-page.ts
@@ -1,33 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../core/services/product.service';
 import { Product } from '../../core/models/product.model';
-import { NgIf } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-catalog-item-page',
-  imports: [NgIf],
+  imports: [NgIf, CommonModule ],
   templateUrl: './catalog-item-page.html',
   styleUrl: './catalog-item-page.scss',
 })
 export class CatalogItemPage { 
-  product?: Product;
-  
-  constructor(
-    private route: ActivatedRoute,
-    private productService: ProductService,
-  ) {
-  }
-  ngOnInit(): void {
-    //Same SPA
-    this.route.paramMap.subscribe(params => {
-      const id = params.get('id');
-      if (id) {
-        this.product = this.productService.getProduct(id);
-      }
-    });
-  }
-  submit(){
-    console.log('SUBMITING')
-  }
+  private readonly route:ActivatedRoute = inject(ActivatedRoute);
+  private readonly productService: ProductService = inject(ProductService);
+
+  readonly product$ = this.route.paramMap.pipe(
+    map(params => params.get('id')),
+    filter((id): id is string => id !== null),
+    map(id => this.productService.getProduct(id))
+  );
 }
