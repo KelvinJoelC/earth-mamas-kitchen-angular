4875f73070b3d0a38d7a46f6ff0ea8550af1d615
Kelvin Carreno <kelvinjcarreno@gmail.com>
2025-12-29 17:13:33 +1000

    feat: integrate Supabase for backend services and add environment configurations

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+
+    supabaseUrl : 'https://xyzcompany.supabase.co',
+    supabaseKey : 'publishable-or-anon-key',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+
+    supabaseUrl : 'https://xyzcompany.supabase.co',
+    supabaseKey : 'publishable-or-anon-key',
+};
