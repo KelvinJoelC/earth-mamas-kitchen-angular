9371d67d859e24052fdaffce84feb12246d053ae
Kelvin Carreno <kelvinjcarreno@gmail.com>
2025-12-30 20:41:28 +1000

    fix: correct variable naming from 'fields' to 'field' in kel-select component

--- src/app/shared/kel-select/kel-select.ts
+++ src/app/shared/kel-select/kel-select.ts
@@ -10,6 +10,6 @@ import { FormGroup, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './kel-select.scss',
 })
 export class KelSelect {
-  @Input({ required: true })  fields!: OptionEntry;
+  @Input({ required: true })  field!: OptionEntry;
   @Input({ required: true })  form!: FormGroup;
 }
