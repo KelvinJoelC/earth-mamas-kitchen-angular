0f87e5b0201c24b5b653d2e691ac7fa294ac6221
Kelvin Carreno <kelvinjcarreno@gmail.com>
2026-01-14 19:32:12 +1000

    correction guard

--- src/app/core/guards/checkout-payment-guard.ts
+++ src/app/core/guards/checkout-payment-guard.ts
@@ -1,11 +1,12 @@
 import { inject } from "@angular/core";
 import { CanActivateFn, Router } from "@angular/router";
+import { CheckoutStore } from "../stores/checkout.store";
 
 export const checkoutDetailsGuard: CanActivateFn = () => {
   const checkout = inject(CheckoutStore);
   const router = inject(Router);
 
-  if (!checkout.isDetailsComplete()) {
+  if (!checkout.isDetailsCompleted()) {
     router.navigate(['/checkout/details']);
     return false;
   }
