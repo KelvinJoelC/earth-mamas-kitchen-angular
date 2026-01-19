4737e4f673596e5272a73e4b3874aedf98d8e197
Kelvin Carreno <kelvinjcarreno@gmail.com>
2026-01-19 19:22:33 +1000

    visual cart update

--- src/app/pages/cart-page/cart-page.ts
+++ src/app/pages/cart-page/cart-page.ts
@@ -5,7 +5,19 @@ import { CartService } from '../../core/services/cart.service';
   standalone: true,
   selector: 'app-cart-page',
   templateUrl: './cart-page.html',
+  styleUrl: './cart-page.scss',
 })
 export class CartPage {
   readonly cart = inject(CartService);
+
+  objectEntries(obj: Record<string, any>) {
+    return Object.entries(obj).map(([key, value]) => ({ key, value }));
+  }
+
+  activeAddOns(addOns: Record<string, boolean>) {
+    return Object.entries(addOns)
+      .filter(([, value]) => value)
+      .map(([key]) => key);
+  }
+
 }
