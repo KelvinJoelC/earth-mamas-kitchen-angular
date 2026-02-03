a8c177472e8e959f5ab019f0a95d0098d553b6d0
Kelvin Carreno <kelvinjcarreno@gmail.com>
2026-02-03 19:53:32 +1000

    visual correction

--- src/app/features/collection/collection-item/collection-item.ts
+++ src/app/features/collection/collection-item/collection-item.ts
@@ -1,14 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Collection } from '../../../core/models/collection.model';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ProductService } from '../../../core/services/product.service';
+
 
 @Component({
   selector: 'app-collection-item',
-  imports: [MatIconModule, RouterLink],
+  imports: [MatIconModule, RouterLink, MatProgressSpinnerModule],
   templateUrl: './collection-item.html',
   styleUrl: './collection-item.scss',
 })
 export class CollectionItem {
   @Input() product!: Collection;
+  
+  readonly productService = inject(ProductService);
 }
